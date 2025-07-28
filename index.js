2e6c11d99d99190dcd72ba073309ade491e7d162
Simran <simrandhital2023@gmail.com>
2025-07-28 11:10:46 +0545

    added second file

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-console.log("This is version 1.0.0 of the index.js file");
-//console.log("This is updated to second version");
+//console.log("This is version 1.0.0 of the index.js file");
+console.log("This is updated to second version");
